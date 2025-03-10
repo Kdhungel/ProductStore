b8ce82fc7e069bd2889e515e1819bd9dea7a78cc
Kritish <kritishdhungel@gmail.com>
2025-03-10 16:09:17 +0545

    Added ChakraUI

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import { Provider } from "@/components/ui/provider";
 
-createRoot(document.getElementById('root')).render(
+import { BrowserRouter } from "react-router-dom";
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <Provider>
+        <App />
+      </Provider>
+    </BrowserRouter>
+  </StrictMode>
+);
